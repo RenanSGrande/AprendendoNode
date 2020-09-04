f783eb874b4fcd986c9fd0cfcc60da599c81bf8e
Renan Grande <renansgrande@gmail.com99078581>
2020-09-04 20:53:50 -0300

    first commit

---
+++ knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : 'Senh@123senh@',
+      database : 'oministack'
+    },
+  migrations: {
+    directory: './src/database/migrations'
+  },
+  useNullAsDefault: true, 
+},
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ src/controllers/SessionController.js
@@ -0,0 +1,20 @@
+const connection = require('../database/connection');
+
+module.exports = {
+
+    async create(req,res){
+        const { id } = req.body;
+
+        const ong = await connection('ongs')
+        .where('id', id)
+        .select('name')
+        .first();
+
+        if(!ong){
+            res.status(404).json({ error: 'Not found'});
+        }
+
+        return res.json(ong);
+    }
+
+}
